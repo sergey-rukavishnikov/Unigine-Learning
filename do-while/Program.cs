c2d3bdbdd90ee08cc1bc707f25b0b1cc3ae12b51
Сергей <sergeyruk312@gmail.com>
2022-10-14 20:45:09 +0300

    Initial

---
+++ do-while/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace do_while // Note: actual namespace depends on the project name.
+{
+    internal static class Program
+    {
+        enum BodyParts
+        {
+            Голова = 0,
+            Ноги = 1,
+            Тело = 2
+        }
+        static void Main(string[] args)
+        {
+            int MansScore = 0, PcScore = 0;
+            int Dif;
+            Random r = new Random();
+            int PcChoiseHit, MansChoiseHit = 0, PcChoiseProtect, MansChoiseProtect = 0;
+            
+            do
+            {
+                PcChoiseProtect = r.Next(2);
+                Console.WriteLine($"Выберете, что защитить: {(int)BodyParts.Голова} = {BodyParts.Голова}\n" +
+                                  $"{(int)BodyParts.Ноги} = {BodyParts.Ноги}\n{(int)BodyParts.Тело} = {BodyParts.Тело}");
+                int.TryParse(Console.ReadLine(), out MansChoiseProtect);
+                PcChoiseHit = r.Next(2);
+                Console.WriteLine($"Выберете, что ударить: {(int)BodyParts.Голова} = {BodyParts.Голова}\n" +
+                                  $"{(int)BodyParts.Ноги} = {BodyParts.Ноги}\n{(int)BodyParts.Тело} = {BodyParts.Тело}");
+                int.TryParse(Console.ReadLine(), out MansChoiseHit);
+                if (MansChoiseProtect != PcChoiseHit)
+                {
+                    ++PcScore;
+                }
+
+                if (MansChoiseHit != PcChoiseProtect)
+                {
+                    ++MansScore;
+                }
+                Console.WriteLine($"Счёт: Вы: {MansScore} | ПК: {PcScore}");
+                Dif = Math.Abs(MansScore - PcScore);
+            }
+            while ((MansScore < 5 && PcScore < 5) || Dif < 2) ;
+
+            Console.WriteLine($"Вы {(MansScore > PcScore? "выиграли" : "проиграли")}!");
+        }
+    }
+}
+
